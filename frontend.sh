721de0e09846b0c074344f0189eb4bdf110e629d
prameela <premi.chekuri@gmail.com>
2022-09-17 16:52:19 +0530

    Variables in log file

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,26 @@
+LOG_FILE=/tmp/frontend
 echo Installing nginx software..
-yum install nginx -y &>>/tmp/frontend
+yum install nginx -y &>>LOG_FILE
 echo Status = $?
 
 echo downloading nginx web content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
 echo Status = $?
 
 cd /usr/share/nginx/html
 
 echo Removing the old web content
-rm -rf * &>>/tmp/frontend
+rm -rf * &>>LOG_FILE
 echo Status = $?
 
 echo Extracting the web content
-unzip /tmp/frontend.zip &>>/tmp/frontend
+unzip /tmp/frontend.zip &>>LOG_FILE
 echo Status = $?
 
-mv frontend-main/static/* . &>>/tmp/frontend
-echo Status = $?
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/frontend
-echo Status = $?
+mv frontend-main/static/* . &>>LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG_FILE
 
 echo Starting Nginx service
-systemctl enable nginx &>>/tmp/frontend
-echo Status = $?
-systemctl restart nginx &>>/tmp/frontend
+systemctl enable nginx &>>LOG_FILE
+systemctl restart nginx &>>LOG_FILE
 echo Status = $?
