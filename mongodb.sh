211bb2bc238b84e5125d153c3640645a9aed4f99
prameela <premi.chekuri@gmail.com>
2022-09-20 00:58:18 +0530

    MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -17,3 +17,20 @@ systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
 echo Status = $?
 
+echo "Downloading mongodb schema"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
+echo Status = $?
+
+cd /tmp
+echo "Extracting mongodb schema file"
+unzip mongodb.zip &>>$LOG_FILE
+
+cd mongodb-main
+
+echo "Load catalogue service schema"
+mongo < catalogue.js &>>$LOG_FILE
+echo Status = $?
+
+echo "Load User service schema"
+mongo < users.js &>>$LOG_FILE
+echo Status = $?
