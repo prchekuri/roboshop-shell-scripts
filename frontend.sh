728ad0250540c89c98e49aa24f7699ee19e10fa2
prameela <premi.chekuri@gmail.com>
2022-09-21 01:41:28 +0530

    Adding one common function StatusCheck to avoid repetition

--- frontend.sh
+++ frontend.sh
@@ -1,26 +1,29 @@
 LOG_FILE=/tmp/frontend
+
+source ./common.sh
+
 echo Installing nginx software..
-yum install nginx -y &>>LOG_FILE
-echo Status = $?
+yum install nginx -y &>>$LOG_FILE
+StatusCheck $?
 
 echo downloading nginx web content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>LOG_FILE
-echo Status = $?
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
+StatusCheck $?
 
 cd /usr/share/nginx/html
 
 echo Removing the old web content
-rm -rf * &>>LOG_FILE
-echo Status = $?
+rm -rf * &>>$LOG_FILE
+StatusCheck $?
 
 echo Extracting the web content
-unzip /tmp/frontend.zip &>>LOG_FILE
-echo Status = $?
+unzip /tmp/frontend.zip &>>$LOG_FILE
+StatusCheck $?
 
-mv frontend-main/static/* . &>>LOG_FILE
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG_FILE
+mv frontend-main/static/* . &>>$LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 
 echo Starting Nginx service
-systemctl enable nginx &>>LOG_FILE
-systemctl restart nginx &>>LOG_FILE
-echo Status = $?
+systemctl enable nginx &>>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE
+StatusCheck $?
