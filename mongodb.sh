b25440224a9d539a21e55e62757feced026d90a2
prameela <premi.chekuri@gmail.com>
2022-09-21 01:24:53 +0530

    Adding one common function StatusCheck to avoid repetition

--- mongodb.sh
+++ mongodb.sh
@@ -1,25 +1,27 @@
 LOG_FILE=/tmp/mongodb
 
+source ./common.sh
+
 echo "Setting up MongoDB Repo"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
-  echo Status = $?
+StatusCheck $?
 
 echo "Installing MongoDB server"
 yum install -y mongodb-org &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "updating mongodb listen address"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo Status = $?
+StatusCheck $?
 
 echo "Starting MOngoDB service"
 systemctl enable mongod &>>$LOG_FILE
 systemctl restart mongod &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Downloading mongodb schema"
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 cd /tmp
 echo "Extracting mongodb schema file"
@@ -29,8 +31,8 @@ cd mongodb-main
 
 echo "Load catalogue service schema"
 mongo < catalogue.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
 
 echo "Load User service schema"
 mongo < users.js &>>$LOG_FILE
-echo Status = $?
+StatusCheck $?
