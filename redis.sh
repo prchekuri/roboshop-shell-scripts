fd52152b4b0d97c1d208e046ce6ef38dbba20b08
prameela <premi.chekuri@gmail.com>
2022-09-21 07:58:22 +0530

    Redis

--- redis.sh
+++ redis.sh
@@ -15,7 +15,7 @@ yum install redis -y &>>$LOG_FILE
 StatusCheck $?
 
 echo "Update Redis listen address from 127.0.0.1 to 0.0.0.0"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 StatusCheck $?
 
 systemctl enable redis
