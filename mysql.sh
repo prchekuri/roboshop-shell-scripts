74246cd1a2e6383584004cb971e736129a6ce746
prameela <premi.chekuri@gmail.com>
2022-09-22 01:23:58 +0530

    MySQL

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,5 @@
 LOG_FILE=/tmp/mysql
+source common.sh
 
 echo "Setting up mysql Repo"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo  &>>$LOG_FILE
