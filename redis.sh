6e7408a9e9f400137b2b4c013613c24882a71fd3
prameela <premi.chekuri@gmail.com>
2022-09-21 07:49:05 +0530

    Redis

---
+++ redis.sh
@@ -0,0 +1,25 @@
+LOG_FILE=/tmp/redis
+
+source common.sh
+
+echo "setup yum repos for redis"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOG_FILE
+StatusCheck $?
+
+echo "Enable redis yum module"
+dnf module enable redis:remi-6.2 -y &>>$LOG_FILE
+StatusCheck $?
+
+echo "Install Redis"
+yum install redis -y &>>$LOG_FILE
+StatusCheck $?
+
+echo "Update Redis listen address from 127.0.0.1 to 0.0.0.0"
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>$LOG_FILE
+StatusCheck $?
+
+systemctl enable redis
+
+echo "Starting Redis service"
+systemctl start redis
+StatusCheck $?
