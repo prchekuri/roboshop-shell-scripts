adfa6cfaf061b9ab45e7aa8ac8729adc82425996
prameela <premi.chekuri@gmail.com>
2022-09-24 22:56:37 +0530

    MySQL

--- common.sh
+++ common.sh
@@ -13,56 +13,79 @@ StatusCheck(){
   fi
 }
 
+APP_PREREQ(){
+  #check whether roboshop user is existing or not. If not, go ahead and add else skip
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+      echo "Add Roboshop Application User"
+      useradd roboshop &>>$LOG_FILE
+     StatusCheck $?
+    fi
+
+    echo "Download ${COMPONENT} Application code"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+    StatusCheck $?
+
+    cd /home/roboshop
+
+    echo "Clean all old App content"
+    rm -rf ${COMPONENT} &>>$LOG_FILE
+    StatusCheck $?
+
+    echo "Extracting ${COMPONENT} Application code"
+    unzip /tmp/${COMPONENT}.zip &>>$LOG_FILE
+    StatusCheck $?
+
+    mv ${COMPONENT}-main ${COMPONENT}
+    cd /home/roboshop/${COMPONENT}
+}
+
+SYSTEMD_SETUP(){
+  echo "Update SystemD service file"
+    sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+    StatusCheck $?
+
+    echo "Setup ${COMPONENT} Service"
+    mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+    StatusCheck $?
+
+    systemctl daemon-reload &>>${LOG_FILE}
+    systemctl enable ${COMPONENT} &>>${LOG_FILE}
+
+    echo "Start ${COMPONENT} Service"
+    systemctl start ${COMPONENT} &>>${LOG_FILE}
+    StatusCheck $?
+}
+
 NODEJS(){
   echo "Setup Nodejs Repos"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>LOG_FILE
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOG_FILE
   StatusCheck $?
 
   echo "Install NodeJS"
-  yum install nodejs -y &>>LOG_FILE
+  yum install nodejs -y &>>$LOG_FILE
   StatusCheck $?
 
-  #check whether roboshop user is existing or not. If not, go ahead and add else skip
-  id roboshop &>>LOG_FILE
-  if [ $? -ne 0 ]; then
-    echo "Add Roboshop Application User"
-    useradd roboshop &>>LOG_FILE
-   StatusCheck $?
-  fi
-
-  echo "Download ${COMPONENT} Application code"
-  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>LOG_FILE
-  StatusCheck $?
-
-  cd /home/roboshop
-
-  echo "Clean all old App content"
-  rm -rf ${COMPONENT} &>>LOG_FILE
-  StatusCheck $?
-
-  echo "Extracting ${COMPONENT} Application code"
-  unzip /tmp/${COMPONENT}.zip &>>LOG_FILE
-  StatusCheck $?
-
-  mv ${COMPONENT}-main ${COMPONENT}
-  cd /home/roboshop/${COMPONENT}
+  APP_PREREQ
 
   echo "Install NodeJS dependencies"
-  npm install &>>LOG_FILE
+  npm install &>>$LOG_FILE
   StatusCheck $?
 
-  echo "Update SystemD service file"
-  sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service
+  SYSTEMD_SETUP
+}
+
+JAVA(){
+  echo "Install Maven"
+  yum install maven -y &>>$LOG_FILE
   StatusCheck $?
 
-  echo "Setup ${COMPONENT} Service"
-  mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>LOG_FILE
+  APP_PREREQ
+
+  echo "Download dependencies and make package"
+  mvn clean package &>>${LOG_FILE}
+  mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar &>>${LOG_FILE}
   StatusCheck $?
 
-  systemctl daemon-reload &>>LOG_FILE
-  systemctl enable ${COMPONENT} &>>LOG_FILE
-
-  echo "Start ${COMPONENT} Service"
-  systemctl start ${COMPONENT} &>>LOG_FILE
-  StatusCheck $?
+  SYSTEMD_SETUP
 }
